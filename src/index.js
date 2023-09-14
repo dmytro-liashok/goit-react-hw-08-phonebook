b1621dd0f22923ce6e5d4463e6f3d9b7c0e7f60c
lyashodm <lyashokdima1@gmail.com>
2023-09-14 10:37:43 +0300

    sec

--- src/index.js
+++ src/index.js
@@ -11,11 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistedStore}>
-        <BrowserRouter >
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-// basename = '/goit-react-hw-08-phonebook';
